2385a5cb06f5002e85307eb0bbe9cdab815970df
Shubham Guthale <150772720+ShubhamGuthale@users.noreply.github.com>
2024-01-28 19:11:33 +0530

    Create dsa21.java

---
+++ Recursion/dsa21.java
@@ -0,0 +1,23 @@
+//sum of n natural number using recursion
+
+public class dsa21 {
+    public static void printSum(int i,int n,int sum)
+    {
+        if (i==n)
+        {
+            sum +=i;
+            System.out.println(sum);
+            return;
+        }
+        sum +=i;
+        printSum(i+1,n,sum);
+       // System.out.println(i);
+    }
+
+
+    public static void main(String args[])
+    {
+        printSum(1,5,0);
+    }
+}
+
