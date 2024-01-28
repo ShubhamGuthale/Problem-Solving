166019ed9ad3f46b3d322bf8179b3e0b4f835cbc
Shubham Guthale <150772720+ShubhamGuthale@users.noreply.github.com>
2024-01-28 19:46:50 +0530

    Create prg7.java

---
+++ Scanner Class/prg7.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+class prg7
+{
+   public static void main(String args[])
+   {
+      Scanner i = new Scanner(System.in);
+      System.out.println("enter some input : ");
+      int a = i.nextInt();
+      System.out.println("this is your input: ");
+   }
+}
