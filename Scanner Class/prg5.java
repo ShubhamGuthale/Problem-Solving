091f017b93f92cbddd40a30bcffeb54d918bac95
Shubham Guthale <150772720+ShubhamGuthale@users.noreply.github.com>
2024-01-28 19:47:29 +0530

    Create prg5.java

---
+++ Scanner Class/prg5.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+class prg5
+{
+    public static void main(String args[])
+    {
+        Scanner b = new Scanner(System.in);
+        int a = b.nextInt();
+        System.out.println("this is your input:\t"+ a);
+        
+    
+    }
+}
