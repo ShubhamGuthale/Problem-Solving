a7903be17036fc59c1e74c78134ea1fc15366035
Shubham Guthale <150772720+ShubhamGuthale@users.noreply.github.com>
2024-01-28 19:20:48 +0530

    Create dsa5.java

---
+++ Functions/dsa5.java
@@ -0,0 +1,17 @@
+//Function
+
+import java.util.Scanner;
+
+public class dsa5 {
+public static void main(String[] args) {
+    Scanner in=new Scanner(System.in);
+    String name = in.nextLine();
+    PrintMyName(name);
+}
+
+public static void PrintMyName(String name)
+{
+    System.out.println(name);
+}
+    
+}
