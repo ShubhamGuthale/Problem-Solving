e7d896e79d02aa2390b6991af8a50ec2faa14aed
Shubham Guthale <150772720+ShubhamGuthale@users.noreply.github.com>
2024-01-28 21:50:46 +0530

    Create prg.cpp

---
+++ C++/prg.cpp
@@ -0,0 +1,21 @@
+//command for run this cpp program ----> g++ prg.cpp -o prg
+//                  ./prg
+// make sure window security virus and thread protection issue many time
+
+#include<iostream>
+using namespace std;
+int main()
+{
+int package;
+cin>>package;
+if(package>10)
+{
+    cout<<"ok";
+}
+else
+{
+    cout<<"study";
+}
+return 0;
+}
+
