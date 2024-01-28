c643ca9d768fe2fdcc20bc637ac473b7d62439fa
Shubham Guthale <150772720+ShubhamGuthale@users.noreply.github.com>
2024-01-28 21:39:38 +0530

    Create prg.java

---
+++ Basic/prg.java
@@ -0,0 +1,9 @@
+class prg
+{  
+    public static void main(String[] args)
+//store a roll number
+int rno=10;
+//store a person name
+String name="My Name";
+System.out.println("Name is are : "+ name + "Roll No is : "+ rno); 
+}
