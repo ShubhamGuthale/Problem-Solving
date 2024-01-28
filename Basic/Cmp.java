cd886e9b7c9c5e2dc8634aefc21c50cee7943876
Shubham Guthale <150772720+ShubhamGuthale@users.noreply.github.com>
2024-01-28 21:15:38 +0530

    Create Cmp.java

---
+++ Basic/Cmp.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+/**
+ * prg11
+ */
+public class Cmp {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        String a="i"; 
+        String b="i"; 
+    System.out.println(a==b);
+}
+    }
