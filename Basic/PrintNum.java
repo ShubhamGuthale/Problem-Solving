69ab8a3361da14f7f31b0b08a72613b453b2ccaa
Shubham Guthale <150772720+ShubhamGuthale@users.noreply.github.com>
2024-01-28 18:31:34 +0530

    Update PrintNum.java

--- Basic/PrintNum.java
+++ Basic/PrintNum.java
@@ -1,1 +1,15 @@
+//Give input from user Print Number
+
+import java.util.Scanner;
+class PrintNum
+{
+    public static void main(String args[])
+    { 
+        Scanner in=new Scanner(System.in);
+        System.out.println("Enter any number :");
+        int n=in.nextInt();
+        System.out.println("your enter number is :"+ n);
+    }
+    
+}
 
