457d25a022338ef6b6bdab4d7247cb16104961da
Shubham Guthale <150772720+ShubhamGuthale@users.noreply.github.com>
2024-01-28 21:12:42 +0530

    Create Reverse.java

---
+++ Practice/Reverse.java
@@ -0,0 +1,22 @@
+//reverse number
+
+import java.util.Scanner;
+
+class Reverse
+{
+    public static void main(String args[])
+    {
+        Scanner s =new Scanner(System.in);
+        System.out.println("enter number you want to reverse :");
+         int num=s.nextInt();
+        //int num=1234567;
+        int ans=0;
+        while(num>0)
+        {
+            int rem=num % 10;
+            num /= 10;
+            ans=ans*10+rem;
+        }
+        System.out.println(ans);
+    }
+}
