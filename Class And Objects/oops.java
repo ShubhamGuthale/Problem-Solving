c8a35ea7a31efcbdb459bf5f790ea572bf470353
Shubham Guthale <150772720+ShubhamGuthale@users.noreply.github.com>
2024-02-04 12:01:16 +0530

    Update oops.java

--- Class And Objects/oops.java
+++ Class And Objects/oops.java
@@ -6,6 +6,10 @@ class pen
     {
         System.out.println("Color is : "+ color +", Type is : "+type);
     }
+    public void printcolor()
+    {
+        System.out.println(this.color);//this keyword
+    }
 }
 public class oops {
     public static void main(String args[])
@@ -14,6 +18,11 @@ public class oops {
         p1.color="blue";
         p1.type="gel";
         p1.display(); //method call
+        pen p2=new pen();
+        p2.color="black";
+        p2.type="marker";
+        p1.printcolor();
+        p2.printcolor();
     }
     
 }
