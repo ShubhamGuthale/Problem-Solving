e62b1b6661feb85abd36a07ccf27b863cd6235fe
Shubham Guthale <150772720+ShubhamGuthale@users.noreply.github.com>
2024-02-06 14:17:30 +0530

    Update ArrayList.java

--- Collection Framework/ArrayList.java
+++ Collection Framework/ArrayList.java
@@ -2,11 +2,17 @@ import java.util.*;
 public class dsa36 {
     public static void main(String[] args) {
      ArrayList<Integer> a1=new ArrayList<Integer>();
+        //add element
      a1.add(1);
      a1.add(2);
      a1.add(3);
      System.out.println(a1);
+        
+         //get element
+       int element=a1.get(0);
+       System.out.println(element);
     }
     
 }
 //Output: [1, 2, 3]
+//           1
