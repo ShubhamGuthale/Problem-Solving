a4b65ac201165d999deeac3ab366ceaca849111c
Shubham Guthale <150772720+ShubhamGuthale@users.noreply.github.com>
2024-02-06 14:40:25 +0530

    Update ArrayList.java

--- Collection Framework/ArrayList.java
+++ Collection Framework/ArrayList.java
@@ -1,18 +1,49 @@
 import java.util.*;
 public class dsa36 {
     public static void main(String[] args) {
-     ArrayList<Integer> a1=new ArrayList<Integer>();
-        //add element
-     a1.add(1);
-     a1.add(2);
-     a1.add(3);
-     System.out.println(a1);
-        
-         //get element
+        Arraya1<Integer> a1=new Arraya1<Integer>();
+        a1.add(1);
+        a1.add(2);
+        a1.add(3);
+        System.out.println(a1);
+
+       //get element
        int element=a1.get(0);
        System.out.println(element);
-    }
-    
-}
-//Output: [1, 2, 3]
-//           1
+
+       //add element perticular position
+       a1.add(1,1);//index:1,element:1
+       System.out.println(a1);
+       
+       //set element
+       a1.set(0,5);//index:0,element:5
+       System.out.println(a1);
+
+        //delete elements
+       a1.remove(0); // 0 is the index
+       System.out.println(a1);
+
+       //size of ArrayList
+       int size = a1.size();
+       System.out.println(size);
+
+       //Loops on ArrayList
+       for(int i=0; i<a1.size(); i++)
+           {
+           System.out.print(a1.get(i) + " ");
+            }
+       System.out.println();
+        
+       //Sorting the ArrayList
+       a1.add(0);
+       Collections.sort(a1); //import java.util.Collection 
+       System.out.println(a1); 
+     }
+   }
+
+/*Output: [1, 2, 3]
+           1
+           .
+           .
+           .
+*/
