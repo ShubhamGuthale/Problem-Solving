f79f8e3994e260fa3be6c08b4ca07b199ebd0f25
Shubham Guthale <150772720+ShubhamGuthale@users.noreply.github.com>
2024-02-06 14:10:35 +0530

    Create ArrayList.java

---
+++ Collection Framework/ArrayList.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class dsa36 {
+    public static void main(String[] args) {
+     ArrayList<Integer> a1=new ArrayList<Integer>();
+     a1.add(1);
+     a1.add(2);
+     a1.add(3);
+     System.out.println(a1);
+    }
+    
+}
+//Output: [1, 2, 3]
