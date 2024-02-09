1e63eef8a062a13180f4eed2b5b4a9248431f62a
Shubham Guthale <150772720+ShubhamGuthale@users.noreply.github.com>
2024-02-09 19:30:32 +0530

    Update Diameter Of Tree.java

--- Tree/Diameter Of Tree.java
+++ Tree/Diameter Of Tree.java
@@ -1,4 +1,4 @@
-//Diameter of Tree - Approach1 O(N^2)
+//Diameter of Tree - Approach1 O(N^2) and  Approach2 O(N)
 //No.of nodes in the longest path between any 2 nodes.
 public class dsa52 {
     static class Node {
@@ -49,13 +49,29 @@ public static int height(Node root) {
         return 0;
     }
 
-
     int diam1 = height(root.left) + height(root.right) + 1;
     int diam2 = diameter(root.left);
     int diam3 = diameter(root.right);
 
     return Math.max(diam1, Math.max(diam2, diam3));
  }
+    
+// Diameter of Tree - Approach2 O(N)
+ public static TreeInfo diameter(Node root) 
+    {
+       if(root == null) 
+       {
+           return new TreeInfo(0, 0);
+       }
+       TreeInfo leftTI = diameter(root.left);
+       TreeInfo rightTI = diameter(root.right);
+       int myHeight = Math.max(leftTI.height, rightTI.height) + 1;
+       int diam1 = leftTI.height + rightTI.height + 1;
+       int diam2 = leftTI.diam;
+       int diam3 = rightTI.diam;
+       int myDiam = Math.max(diam1, Math.max(diam2, diam3));
+       return new TreeInfo(myHeight, myDiam);
+   }
 
     public static void main(String args[])
      {
