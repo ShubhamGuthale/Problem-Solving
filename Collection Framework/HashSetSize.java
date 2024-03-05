599362b1f55e0bb0f244fd7d46ea067b9096509a
Shubham Guthale <150772720+ShubhamGuthale@users.noreply.github.com>
2024-03-05 16:27:18 +0530

    Create HashSetSize.java

---
+++ Collection Framework/HashSetSize.java
@@ -0,0 +1,40 @@
+//Hashing - HashSet
+import java.util.HashSet;
+class dsa61
+{
+    public static void main(String args[])
+    {//Insertion
+        HashSet <Integer> set=new HashSet<>();
+        set.add(1);
+        set.add(2);
+        set.add(4);
+        set.add(3);
+        System.out.println(set);
+        
+        //search - contains or not.
+        if(set.contains(1))
+        {
+            System.out.println("set contain 1");
+        }
+        
+        //Delete
+        set.remove(1);
+        if(!set.contains(1))
+        {
+            System.out.println("we deleted 1");
+        }
+        System.out.println(set);
+        
+        //Size
+        System.out.println("size of set is :"+set.size());
+
+
+    }
+}
+
+//Output : 
+//[1, 2, 3, 4]
+//set contain 1
+//we deleted 1
+//[2, 3, 4]
+//size of set is :3
