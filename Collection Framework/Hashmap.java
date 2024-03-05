46bee232784e5e4ebde660a2e95c093672113918
Shubham Guthale <150772720+ShubhamGuthale@users.noreply.github.com>
2024-03-05 15:30:30 +0530

    Create Hashmap.java

---
+++ Collection Framework/Hashmap.java
@@ -0,0 +1,17 @@
+//Hashing - Hashmap
+import java.util.HashSet;
+class dsa58
+{
+    public static void main(String args[])
+    {
+        HashSet <Integer> set=new HashSet<>();
+        set.add(1);
+        set.add(2);
+        set.add(4);
+        set.add(3);
+        System.out.println(set);
+
+    }
+}
+
+Output : [1, 2, 3, 4]  --- Assending order
