d609b68f834134a0d286938c929accc79ea12562
Shubham Guthale <150772720+ShubhamGuthale@users.noreply.github.com>
2024-03-08 18:16:17 +0530

    Create HashMapInsert.java

---
+++ Collection Framework/HashMapInsert.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class dsa63 {
+    public static void main(String args[])
+    {
+        //country(key), population(value)
+        HashMap<String,Integer> map=new HashMap<>();
+        //Insertion
+        map.put("India",120);
+        map.put("US",30);
+        map.put("China",150);
+        System.out.println(map);
+    }
+}
+//{China=150, US=30, India=120}
