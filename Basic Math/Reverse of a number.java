5d50d08a3afa895ec0e0922ab03ffbb2f27397df
Shubham Guthale <150772720+ShubhamGuthale@users.noreply.github.com>
2024-05-27 16:01:24 +0530

    Create  Reverse of a number.java

---
+++ Basic Math/Reverse of a number.java
@@ -0,0 +1,24 @@
+// Reverse of a number - code 360 coding ninjas
+import java.util.*;
+public class Main
+ {
+	static int revers(int n) 
+    { 
+        int revn = 0; 
+        while(n > 0) 
+        { 
+            revn = revn * 10 + n% 10; 
+            n = n/ 10; 
+        } 
+        return revn; 
+	}
+	
+	public static void main(String[] args) 
+	{
+		// Write your code here
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt(); 
+        System.out.println(revers(n)); 
+    } 
+}
+
